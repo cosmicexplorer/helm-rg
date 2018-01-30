11f8b236eef5f28ebc18f371a9624c7ed4c58859
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-01-30 00:57:16 -0800

    make the header conform to package-lint

--- rg3.el
+++ rg3.el
@@ -1,4 +1,4 @@
-;;; rg3.el --- RipGrep is Great: a helm interface for searching file content really fast -*- lexical-binding: t -*-
+;;; rg3.el --- a helm interface to ripgrep -*- lexical-binding: t -*-
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
