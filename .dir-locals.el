b1043488af71c1361836d114b49529130ab05b7c
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-06-28 22:43:30 -0700

    add some dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode
-  (fill-column . 100)))
+((nil
+  (require-final-newline . t)
+  (indent-tabs-mode . nil)
+  (checkdoc-force-docstrings-flag . nil))
+ (emacs-lisp-mode
+  (sentence-end-double-space . nil)))
