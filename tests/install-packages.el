b0a12e4f5bbde1ab5b55a983a95054e7ba0799c7
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-06-29 00:31:42 -0700

    specify package-archives for travis

--- tests/install-packages.el
+++ tests/install-packages.el
@@ -1,10 +1,12 @@
 (require 'cl-lib)
 
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.milkbox.net/packages/")))
+
 (package-initialize)
 
-(defconst dependent-packages
-  '(dash
-    helm))
+(defconst dependent-packages '(dash helm))
 
 (cl-loop for pkg in dependent-packages
          do (package-install pkg))
